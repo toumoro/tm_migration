951ed5c917806c4c1e562e9091cf7bf1648c38f4
tm-hdaoud <haythem.daoud@toumoro.com>
2025-06-16 10:46:17 +0000

    [TASK] ajout d'un upgrade wizard pour la migration des scripts SQL

---
+++ Classes/Service/SqlMigrationService.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Toumoro\TmMigration\Service;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class SqlMigrationService
+ */
+class SqlMigrationService
+{
+    /**
+     * @param array $queries
+     *
+     * @return int
+     */
+    public function migrate(array $queries): int
+    {
+        $count = 0;
+
+        foreach ($queries as $sql) {
+
+            if (empty(trim($sql))) {
+                continue;
+            }
+
+            try {
+                $connection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionByName('Default');
+                $connection->executeStatement($sql);
+                $count++;
+            } catch (\Exception $e) {
+                $logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
+                $logger->error('SQL migration error : ', [
+                    'query' => $sql,
+                    'message' => $e->getMessage(),
+                ]);
+            }
+        }
+
+        return $count;
+    }
+}
---
+++ migration.sql
@@ -0,0 +1,3 @@
+UPDATE tt_content SET header = 'New header text 1' WHERE uid = 123;
+UPDATE tt_content SET subheader = 'New subheader text 1' WHERE uid = 123;
+DELETE FROM tt_content WHERE uid = 123;
